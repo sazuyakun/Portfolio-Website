bc159283840016fb9358823e938eb3d83e3f7f7a
Soham Samal <sohamsamal37@gmail.com>
2023-12-07 16:49:52 +0530

    some Hero changes

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -28,15 +28,23 @@ const Hero = () => (
       </div>
         <motion.div 
           variants = {slideIn('right', 'tween', 0.2, 1)}
-          className = "relative w-full md:-mt-[20px] -mt-[12px]"
+          className = "relative w-full md:-mt-[100px] -mt-[50px]"
         >
-        <div className = "absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]">
+        <div className = "absolute w-full h-[300px] hero-gradient rounded-tl-[140px] rounded-bl-[140px]  z-[0] -top-[10px]" />
         <img
           src='/cover.png' 
           alt='cover'
-          className=' w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative'
+          className=' w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] rounded-bl-[140px] z-10 relative '
         />
-        </div>
+        <a href='#explore'>
+          <div className=' w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10'>
+            <img 
+              src='/stamp.png'
+              alt='stamp'
+              className=' sm:w-[155px] w-[100px] sm:h-[150px] h-[100px] object-contain'
+            />
+          </div>
+        </a>
         </motion.div>
     </motion.div>
   </section>
